e9b3ae9c7a9d4eafad0c0f3eba79b8e427f3c8c8
abishekkumar565 <abishekkumar565@gmail.com>
2025-01-05 23:17:04 +0100

    changes as on 5th jan 11:17pmnight

---
+++ cypress/e2e/CypressUIAutomation/FirstJSProgram.cy.js
@@ -0,0 +1,15 @@
+describe('Verify Login', () => { //test Scenarios
+
+    it('Valid Username and password - 1', () => {//Multiple test cases 
+        cy.log("Valid credentials - 1")
+    })
+
+    it('Valid Username and password - 2', () => {//Multiple test cases 
+        cy.log("Valid credentials - 2")
+    })
+
+    it('Valid Username and password - 3', function () {//Multiple test cases 
+        cy.log("Valid credentials - 3")
+    })
+
+})
